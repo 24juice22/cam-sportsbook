92051eb46e5f4a528fa324665c8177b81019cb3c
24juice22 <mike.james.nunn@gmail.com>
2022-08-08 12:52:40 -0400

    Navbar CSS Styling
    Added CSS Styling to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,22 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <div className="container">
+            <div className="container container-navbar">
                 <div className="navbar__top">
                     <a href="#" className="navbar__brand">
                         <h3 className="navbar__logo">CAM</h3>
                         Sportsbook
                     </a>
                     <div className="buttons">
-                        <button className="btn navbar__button">Log In</button>
-                        <button className="btn navbar__button">JOIN NOW</button>
+                        <button className="btn navbar__button navbar__button-login">Log In</button>
+                        <button className="btn navbar__button navbar__button-join">JOIN NOW</button>
                     </div>
                 </div>
                 <div className="navbar__bottom">
-                    <ul className="nav">
-                        <li className="nav__item">MLB</li>
-                        <li className="nav__item">NFL</li>
-                        <li className="nav__item">NCAAF</li>
+                    <ul className="navbar__bottom-nav">
+                        <li className="nav__item"><a className="nav-link" href="">MLB</a></li>
+                        <li className="nav__item"><a className="nav-link" href="">NFL</a></li>
+                        <li className="nav__item"><a className="nav-link" href="">NCAAF</a></li>
                     </ul>
                 </div>
             </div>
