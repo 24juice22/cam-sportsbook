8fc5aee02163e312ef5acb74909babd66772fc79
24juice22 <mike.james.nunn@gmail.com>
2022-08-16 11:06:03 -0400

    Added API and Box Component to Display Data
    Called a sportsbook odds api that returns mlb data. Each mlb matchup gets mapped over and the data is passed as props to the new "Box" component. The box component will eventually display the teams and betting odds for each matchup.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react"
+import React from "react";
 import Navbar from "./components/Navbar";
-import Betbar from "./components/Betbar"
+import Betbar from "./components/Betbar";
+import Box from "./components/Box";
 
 function App() {
+  const [games, setGames] = React.useState(null)
+
+  React.useEffect(() => {
+    fetch("https://api.the-odds-api.com/v4/sports/baseball_mlb/odds/?regions=us&markets=spreads,totals,h2h&oddsFormat=american&apiKey=7e633aea1cc34e3ceec88cb2bb5d135d")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setGames(data);
+    }) 
+  }, [])
+
   return (
     <>
       <Navbar />
+      {games.map(matchup => <Box matchup={matchup}/>)}
       <Betbar />
     </>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Box.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Box({matchup}) {
+    return (
+        <h1>{matchup.away_team}</h1>
+    )
+}
