abeb607d72fbe6a248e6217760e0bf64423ba353
24juice22 <mike.james.nunn@gmail.com>
2022-08-17 06:27:30 -0400

    Updated Box Component Logic
    Updated the box component to now find the correct index for all API data. Some betting lines are indexed differently for different games and this accounts for that. Some betting lines are also non-existent for certain games and this also accounts for that.

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,42 +1,58 @@
 import React from "react"
-import ArizonaDiamondbacks from "../images/mlb/Arizona-Diamondbacks.png"
-import AtlantaBraves from "../images/mlb/Atlanta-Braves.png"
-import BaltimoreOrioles from "../images/mlb/Baltimore-Orioles.png"
-import BostonRedSox from "../images/mlb/Boston-Red-Sox.png"
-import ChicagoCubs from "../images/mlb/Chicago-Cubs.png"
-import ChicagoWhiteSox from "../images/mlb/Chicago-White-Sox.png"
-import CincinnatiReds from "../images/mlb/Cincinnati-Reds.png"
-import ClevelandGuardians from "../images/mlb/Cleveland-Guardians.png"
-import ColoradoRockies from "../images/mlb/Colorado-Rockies.png"
-import DetroitTigers from "../images/mlb/Detroit-Tigers.png"
-import HoustonAstros from "../images/mlb/Houston-Astros.png"
-import KansasCityRoyals from "../images/mlb/Kansas-City-Royals.png"
-import LosAngelesAngels from "../images/mlb/Los-Angeles-Angels.png"
-import MiamiMarlins from "../images/mlb/Miami-Marlins.png"
-import MilwaukeeBrewers from "../images/mlb/Milwaukee-Brewers.png"
-import MinnesotaTwins from "../images/mlb/Minnesota-Twins.png"
-import NewYorkMets from "../images/mlb/New-York-Mets.png"
-import NewYorkYankees from "../images/mlb/New-York-Yankees.png"
-import OaklandAthletics from "../images/mlb/Oakland-Athletics.png"
-import PhiladelphiaPhillies from "../images/mlb/Philadelphia-Phillies.png"
-import PittsburghPirates from "../images/mlb/Pittsburgh-Pirates.png"
-import SanDiegoPadres from "../images/mlb/San-Diego-Padres.png"
-import SanFranciscoGiants from "../images/mlb/San-Francisco-Giants.png"
-import SeattleMariners from "../images/mlb/Seattle-Mariners.png"
-import StLouisCardinals from "../images/mlb/St.-Louis-Cardinals.png"
-import TampaBayRays from "../images/mlb/Tampa-Bay-Rays.png"
-import TexasRangers from "../images/mlb/Texas-Rangers.png"
-import TorontoBlueJays from "../images/mlb/Toronto-Blue-Jays.png"
-import WashingtonNationals from "../images/mlb/Washington-Nationals.png"
-
 
 export default function Box({matchup}) {
+    let spreadExists = null;
+    let totalExists = null;
+    let moneylineExists = null;
+
+    let unibetIndex = matchup.bookmakers.findIndex(item => item.key === 'unibet');
+    if (unibetIndex < 0) unibetIndex = 0;
+    let indexSpread = matchup.bookmakers[unibetIndex].markets.findIndex(item => item.key === 'spreads');
+    let indexTotal = matchup.bookmakers[unibetIndex].markets.findIndex(item => item.key === 'totals');
+    let indexMoneyline = matchup.bookmakers[unibetIndex].markets.findIndex(item => item.key === 'h2h');
+
+/* Point Spreads */
+    if (indexSpread >= 0) spreadExists = true;
+    if (indexSpread < 0) indexSpread = indexMoneyline;
+    if (indexSpread < 0) indexSpread = indexTotal;
+    
     let awayTeam = matchup.away_team;
     let awayTeamImageName = awayTeam.split(" ").join("-");
+    let indexSpreadAwayTeam = matchup.bookmakers[unibetIndex].markets[indexSpread].outcomes.findIndex(item => item.name === awayTeam)
+    let spreadPointAway = matchup.bookmakers[unibetIndex].markets[indexSpread].outcomes[indexSpreadAwayTeam].point;
+    if (spreadPointAway > 0)
+        spreadPointAway = `+${spreadPointAway}`;
+
     console.log(awayTeamImageName);
     let homeTeam = matchup.home_team;
     let homeTeamImageName = homeTeam.split(" ").join("-");
-    let index = matchup.bookmakers.findIndex(item => item.key === 'unibet');
+    let indexSpreadHomeTeam = matchup.bookmakers[unibetIndex].markets[indexSpread].outcomes.findIndex(item => item.name === homeTeam);
+    let spreadPointHome = matchup.bookmakers[unibetIndex].markets[indexSpread].outcomes[indexSpreadHomeTeam].point;
+    if (spreadPointHome > 0)
+        spreadPointHome = `+${spreadPointHome}`;
+
+/* Totals */
+    if (indexTotal >= 0) totalExists = true;
+    if (indexTotal < 0) indexTotal = indexMoneyline;
+    if (indexTotal < 0) indexTotal = indexSpread;
+
+    let indexTotalOver = matchup.bookmakers[unibetIndex].markets[indexTotal].outcomes.findIndex(item => item.name === 'Over')
+    let totalPoint = matchup.bookmakers[unibetIndex].markets[indexTotal].outcomes[indexTotalOver].point;
+
+/* Moneyline */
+    if (indexMoneyline >= 0) moneylineExists = true;
+    if (indexMoneyline < 0) indexMoneyline = indexTotal;
+    if (indexMoneyline < 0) indexMoneyline = indexSpread;
+
+    let indexMoneylineAway = matchup.bookmakers[unibetIndex].markets[indexMoneyline].outcomes.findIndex(item => item.name === awayTeam)
+    let moneylinePriceAway = matchup.bookmakers[unibetIndex].markets[indexMoneyline].outcomes[indexMoneylineAway].price;
+    if (moneylinePriceAway > 0)
+        moneylinePriceAway = `+${moneylinePriceAway}`;
+    
+    let indexMoneylineHome = matchup.bookmakers[unibetIndex].markets[indexMoneyline].outcomes.findIndex(item => item.name === homeTeam)
+    let moneylinePriceHome = matchup.bookmakers[unibetIndex].markets[indexMoneyline].outcomes[indexMoneylineHome].price;
+    if (moneylinePriceHome > 0)
+        moneylinePriceHome = `+${moneylinePriceHome}`;
 
     return (
         <div className="box">
@@ -44,18 +60,19 @@ export default function Box({matchup}) {
                 <img className="team-logo" src={require(`../images/mlb/${awayTeamImageName}.png`)}></img>
                 <p className="box__team-name">{awayTeam}</p>
                 <div className="box__lines">
-                    <p className="line line__spread">{matchup.bookmakers[index].markets[1].outcomes[0].point}</p>
-                    <p className="line line__total">{matchup.bookmakers[index].markets[2].outcomes[0].point}</p>
-                    <p className="line line__moneyline">{matchup.bookmakers[index].markets[0].outcomes[0].price}</p>
+                    {spreadExists ? <p className="line line__spread">{spreadPointAway}</p> : <p className="line line__spread"></p>}
+                    {totalExists ? <p className="line line__total">{`O ${totalPoint}`}</p> : <p className="line line__total"></p>}
+                    {moneylineExists ? <p className="line line__moneyline">{moneylinePriceAway}</p> : <p className="line line__moneyline"></p>}
+                    
                 </div>
             </div>
             <div className="box__team box__home">
                 <img className="team-logo" src={require(`../images/mlb/${homeTeamImageName}.png`)}></img>
                 <p className="box__team-name">{homeTeam}</p>
                 <div className="box__lines">
-                    <p className="line line__spread">{matchup.bookmakers[index].markets[1].outcomes[1].point}</p>
-                    <p className="line line__total">{matchup.bookmakers[index].markets[2].outcomes[1].point}</p>
-                    <p className="line line__moneyline">{matchup.bookmakers[index].markets[0].outcomes[1].price}</p>
+                    {spreadExists ? <p className="line line__spread">{spreadPointHome}</p> : <p className="line line__spread"></p>}
+                    {totalExists ? <p className="line line__total">{`U ${totalPoint}`}</p> : <p className="line line__total"></p>}  
+                    {moneylineExists ? <p className="line line__moneyline">{moneylinePriceHome}</p> : <p className="line line__moneyline"></p>}
                 </div>
             </div>
         </div>
