0c27000f552e9247e07b5e2fa6569f6a0b6cbd44
24juice22 <mike.james.nunn@gmail.com>
2022-08-20 10:03:57 -0400

    Added Betslip Link to Betbar
    Added link to "betslip" page in the Betbar

--- src/components/Betbar.js
+++ src/components/Betbar.js
@@ -1,14 +1,14 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-export default function NavbarBottom() {
+export default function Betbar() {
     return (
         <nav className="betbar">
             <div className="container">
                 <ul className="list betbar__list">
                     <li className="betbar__list-item"><a className="betbar__link" href="#">Top</a></li>
                     <li className="betbar__list-item">
-                        <Link className="betbar__link" to="/betslip">Betslip</Link>
+                        <Link className="betbar__link betslip__link" to="/betslip">Betslip</Link>
                     </li>
                     <li className="betbar__list-item"><a className="betbar__link" href="#">My Bets</a></li>
                 </ul>
