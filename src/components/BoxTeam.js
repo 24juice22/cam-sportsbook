715572e358e013e12afb816500ae3c3088b52178
24juice22 <mike.james.nunn@gmail.com>
2022-08-22 01:58:43 -0400

    Functionality for Clicking a Line
    Created functionality for if a specific betting line is clicked. The background will turn to the secondary color and I will capture the "price" and "point" betting data. I accomplished all of this by creating state in this component that is made up of an array of 3 objects, representing the spread, total, and moneyline. This allowed me to create a "isClicked" key that could be toggled with a click event.

--- src/components/BoxTeam.js
+++ src/components/BoxTeam.js
@@ -1,7 +1,6 @@
 import React from "react"
-import Spread from "./Spread"
-import Total from "./Total"
-import Moneyline from "./Moneyline"
+import Lines from "./Lines"
+
 
 function BoxTeam({team, teamImageName, matchup, totalTeam}) {
     let unibetIndex = matchup.bookmakers.findIndex(item => item.key === 'unibet');
@@ -10,20 +9,65 @@ function BoxTeam({team, teamImageName, matchup, totalTeam}) {
     let indexSpread = betMarkets.findIndex(item => item.key === 'spreads');
     let indexTotal = betMarkets.findIndex(item => item.key === 'totals');
     let indexMoneyline = betMarkets.findIndex(item => item.key === 'h2h');
+    
+    const [bettingLines, setBettingLines] = React.useState(allNewLines())
 
-    let over = 'Over';
-    let under = 'Under';
+    function wasClicked(id, point, price) {
+        setBettingLines(oldLines => oldLines.map(line => {
+            return line.id === id ?
+                {...line, isClicked: !line.isClicked} :
+                line
+        }))
+        console.log(id, point, price)
+    }
+
+
+    function allNewLines() {
+        const newLines = [   
+            {
+                index: indexSpread,
+                team: team,
+                isClicked: false,
+                id: `spread-${team}`
+            },
+            {
+                index: indexTotal,
+                team: totalTeam,
+                isClicked: false,
+                id: `total-${team}`
+            },
+            {
+                index: indexMoneyline,
+                team: team,
+                isClicked: false,
+                id: `moneyline-${team}`
+            }
+        ];
+        return newLines
+    }
+
+    console.log(bettingLines);
+    const lineElements = bettingLines.map(line => (
+        <Lines 
+            key={line.id}
+            index={line.index}
+            isClicked={line.isClicked}
+            betMarkets={betMarkets}
+            team={line.team}
+            id={line.id}
+            wasClicked={wasClicked}
+        />
+    ))
+
 
     return (
-    <div className="box__team">
-        {matchup.sport_title !== 'NCAAF' ? <img className="team-logo" src={require(`../images/${matchup.sport_title.toLowerCase()}/${teamImageName}.png`)}></img> : <img className="team-logo" src=""></img>}
-        <p className="box__team-name">{team}</p>
-        <div className="box__lines">
-            <Spread team={team} betMarkets={betMarkets} indexSpread={indexSpread} />
-            <Total team={totalTeam} betMarkets={betMarkets} indexTotal={indexTotal}/>
-            <Moneyline team={team} betMarkets={betMarkets} indexMoneyline={indexMoneyline}/>
-        </div>
-    </div> 
+        <div className="box__team">
+            {matchup.sport_title !== 'NCAAF' ? <img className="team-logo" src={require(`../images/${matchup.sport_title.toLowerCase()}/${teamImageName}.png`)}></img> : <img className="team-logo" src=""></img>}
+            <p className="box__team-name">{team}</p>
+            <div className="box__lines">
+                {lineElements}
+            </div>
+        </div> 
     )
 }
 
