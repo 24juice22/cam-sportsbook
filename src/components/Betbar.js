ea5240ccf7bc71f27cf47f40d9a323da0d4e3f75
24juice22 <mike.james.nunn@gmail.com>
2022-09-08 09:23:50 -0400

    Betbar CSS Styles
    Updated "betbar" styling

--- src/components/Betbar.js
+++ src/components/Betbar.js
@@ -3,12 +3,7 @@ import { Link } from "react-router-dom"
 import { SportsbookContext } from "../contexts/SportsbookContexts"
 
 export default function Betbar() {
-    const {betbarActive, loggedIn} = useContext(SportsbookContext)
-
-    const betbarStyles = {
-        backgroundColor: betbarActive.length ? "var(--color-secondary)" : "var(--color-primary)",
-        color: betbarActive.length ? "var(--color-primary)" : "white"
-    }
+    const {betbarActive, loggedIn} = useContext(SportsbookContext);
 
     const slipCountStyles = {
         visibility: betbarActive.length ? "visible" : "hidden"
@@ -21,12 +16,12 @@ export default function Betbar() {
                     <li className="betbar__list-item">
                         <a className="betbar__link" href="#">Top</a>
                     </li>
-                    <li className="betbar__list-item" style={betbarStyles}>
+                    <li className="betbar__list-item">
                         <p className="betslip__count" style={slipCountStyles}>{betbarActive.length}</p>
-                        <Link className="betbar__link betslip__link" to="/betslip" style={betbarStyles}>Betslip</Link>
+                        <Link className="betbar__link betslip__link" to="/betslip">Betslip</Link>
                     </li>
                     <li className="betbar__list-item">
-                        {loggedIn && <p className="betbar__bankroll">${(loggedIn.bankroll)}</p>}
+                        {loggedIn && <p className="betbar__bankroll" >${(loggedIn.bankroll)}</p>}
                         <Link className="betbar__link" to="/Account">Account</Link>
                     </li>
                 </ul>
