8c7c563dfeb86c66ec5ddefff913941562cfbfb5
24juice22 <mike.james.nunn@gmail.com>
2022-09-09 07:39:00 -0400

    Fixed Point issue in BetConfirm
    Fixed the point issue in the "betConfirm" component

--- src/components/BetConfirmBox.js
+++ src/components/BetConfirmBox.js
@@ -2,10 +2,17 @@ import React from "react"
 import GameDate from "./GameDate"
 
 function BetConfirmBox({item}) {
+    
+    // Fix point Issue with index of type total //
+    let point = item.point;
+    if (item.indexType === "Total") {
+        point = point.split(" ")[1];
+    }
+
     return (
         <div className="bet-confirm__box">
             <div className="slip__line-numbers">
-                <p className="slip__point">{item.team} {item.point}</p>
+                <p className="slip__point">{item.team} {point}</p>
                 <p className="slip__price">{item.price}</p>
             </div>
             <p className="slip__line-type">{item.indexType}</p>
