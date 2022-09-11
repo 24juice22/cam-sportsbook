b3b0c77b33707a650b79f3ab1fc06a3c2870eb85
24juice22 <mike.james.nunn@gmail.com>
2022-09-11 08:44:16 -0400

    Adjust Accounts Page CSS
    Adjust Accounts Page CSS

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -29,10 +29,10 @@ function Account() {
         <div className="account">
             <div className="container--wide">
                 <h1 className="title title--page">Account</h1>
-                <p className="account__name">{loggedIn.first} {loggedIn.last}</p>
-                <p className="account__bankroll">Bankroll: {loggedIn.bankroll}</p>
+                <p className="account__name">{loggedIn.first.toUpperCase()} {loggedIn.last.toUpperCase()}</p>
+                <p className="account__bankroll">Bankroll: ${loggedIn.bankroll.toFixed(2)}</p>
                 <div className="account__bets">
-                    <h2 className="bets__title">My Bets</h2>
+                    <h2 className="bets__title">MY BETS</h2>
                     {loggedIn.bets.length > 0 ? confirmedBetElements : <p>You have not made any bets.</p>}
                 </div>
             </div>
