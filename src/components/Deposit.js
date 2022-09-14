57e7962344d3e8c814fe876e00956bb374d4ec73
24juice22 <mike.james.nunn@gmail.com>
2022-09-14 11:06:51 -0400

    Deposit Input Pattern
    Set a pattern for the deposit input

--- src/components/Deposit.js
+++ src/components/Deposit.js
@@ -13,11 +13,11 @@ function Deposit() {
         event.preventDefault();
         setAccounts(prevValue => prevValue.map(value =>{
             return value.username === loggedIn.username ?
-                {...value, bankroll: value.bankroll + parseInt(event.target[0].value)} :
+                {...value, bankroll: value.bankroll + Number(event.target[0].value)} :
                 value
         }))
         setLoggedIn(prevValue => {
-            return {...prevValue, bankroll: prevValue.bankroll + parseInt(event.target[0].value)}
+            return {...prevValue, bankroll: prevValue.bankroll + Number(event.target[0].value)}
         })
         hideDeposit();
     }
@@ -36,7 +36,8 @@ function Deposit() {
             <div className="container--wide">
             <h2 className="title">Deposit Money</h2>
             <button className="btn btn--exit" onClick={hideDeposit}>X</button>
-            <p classname="deposit__instructions">Please deposit a valid dollar amount.</p>
+            <p classname="deposit__instructions">Please deposit a valid dollar amount. Must be at least $1.00 and less than $10,000.00</p>
+            <p>You must type your amount to 2 decimal places. Ex (13.00)</p>
             <form className="deposit__form" onSubmit={handleDeposit}>
                 <div className="deposit-container">
                 <label className="deposit-label">Deposit Amount $ </label>
@@ -47,6 +48,7 @@ function Deposit() {
                     placeholder="Amount"
                     required
                     autoComplete="off"
+                    pattern="[1-9][0-9]{0,3}(.[0-9]{2})?$"
                 />
                 </div>
                 <button 
