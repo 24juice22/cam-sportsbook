138c010ac70a823ef06df0bef83af9d2fbbf0cd6
24juice22 <mike.james.nunn@gmail.com>
2022-09-14 12:19:53 -0400

    Updated Betbar Styles
    Updated Betbar styles

--- src/components/Betbar.js
+++ src/components/Betbar.js
@@ -24,12 +24,16 @@ export default function Betbar({setSport}) {
                         <a className="betbar__link" href="#">Top</a>
                     </li>
                     <li className="betbar__list-item">
-                        <p className="betslip__count" style={slipCountStyles}>{betbarActive.length}</p>
-                        <Link className="betbar__link betslip__link" to="/betslip">Betslip</Link>
+                        <Link className="betbar__link betslip__link" to="/betslip">
+                            <p className="betslip__count" style={slipCountStyles}>{betbarActive.length}</p>
+                            <p>Betslip</p>
+                        </Link>
                     </li>
                     <li className="betbar__list-item">
-                        {loggedIn && <p className="betbar__bankroll" >${(loggedIn.bankroll)}</p>}
-                        <Link className="betbar__link" to="/Account">Account</Link>
+                        <Link className="betbar__link" to="/Account">
+                            {loggedIn && <p className="betbar__bankroll" >${(loggedIn.bankroll)}</p>}
+                            <p>Account</p>
+                        </Link>
                     </li>
                 </ul>
             </div>
