81210dbb407d0cae38291ffe78881d6833f683bd
24juice22 <mike.james.nunn@gmail.com>
2022-09-25 23:25:24 -0400

    Fixed Mlb-Homepage Large Screen View
    Added appropriate width to the mlb odds that appear on the homepage when viewed on large (1024px) screens.

--- src/pages/Mlb.js
+++ src/pages/Mlb.js
@@ -2,19 +2,19 @@ import React from "react"
 import Box from "../components/Box"
 
 function Mlb({setSport}) {
-    const [baseball, setBaseball] = React.useState(null)
-
+    const [baseball, setBaseball] = React.useState(null);
+   
     React.useEffect(() => {
         fetch("https://api.the-odds-api.com/v4/sports/baseball_mlb/odds/?regions=us&markets=spreads,totals,h2h&oddsFormat=american&apiKey=7e633aea1cc34e3ceec88cb2bb5d135d")
         .then(res => res.json())
         .then(data => {
           setBaseball(data);
-          setSport("MLB")
+          setSport("MLB");
         }) 
     }, [])
 
     if (baseball) return (
-        <div className="page">
+        <div className="page page--home">
             {baseball.map(matchup => <Box matchup={matchup} key={matchup.id}/>)}
         </div>
     )
