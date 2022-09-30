c2295fd5d783437068f7908796c2e86da4d8a6e5
24juice22 <mike.james.nunn@gmail.com>
2022-09-30 07:56:45 -0400

    Update GameDate Component
    GameDate component now updated to account for instance if seconds does not equal ":00".

--- src/components/GameDate.js
+++ src/components/GameDate.js
@@ -1,14 +1,16 @@
 import React from "react"
 
 function GameDate({matchup}) {
-    let date = new Date(matchup.commence_time).toLocaleString('en-US').split(":00 ").join("").split(",").join("");
-    let dateToCompare = date.split(" ")
+    let date = new Date(matchup.commence_time).toLocaleString('en-US').split("");
+    date.splice(date.length - 6, 4);
+    let formattedDate = date.join("").split(",").join("");
+    let dateToCompare = formattedDate.split(" ");
     let currentDate = `${new Date().getMonth() + 1}/${new Date().getDate()}/${new Date().getFullYear()}`;
     if (dateToCompare[0] === currentDate)
-        date = `Today ${dateToCompare[1]}`;
+        formattedDate = `Today ${dateToCompare[1]}`;
 
     return (
-        <p className="date">{date}</p>
+        <p className="date">{formattedDate}</p>
     )
 
 }
